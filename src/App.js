04f2f1528bc68e5f32ece59f53f41590ed1a6be8
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2023-01-14 17:19:40 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./assets/img/logo-teal.svg";
 
 function App() {
-  return (
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://site--backend-deliveroo--97yqlpf4l44b.code.run/"
+      );
+      setData(response.data);
+      console.log(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img className="container" src={logo} alt="logo-teal" />
       </header>
+
+      <section className="restaurant container">
+        <div>
+          <h1>{data.restaurant.name}</h1>
+          <p> {data.restaurant.description}</p>
+        </div>
+        <div>
+          <img src={data.restaurant.picture} alt="pic" />
+        </div>
+      </section>
+      <section className="categories">
+        {data.categories.map((element, index) => {
+          return (
+            <div className="container" key={index}>
+              <h2> {element.name}</h2>
+              <div className="list-parts">
+                {element.meals.map((element2) => {
+                  return (
+                    <div className="parts" key={element2.id}>
+                      <div>
+                        <h3> {element2.title}</h3>
+                        <p>{element2.description}</p>
+                        <h4>{element2.price} €</h4>
+                      </div>
+                      <div>
+                        <img src={element2.picture} alt="pic1" />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
